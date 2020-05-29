16428ca690ed3de8f43bb1d1dbacb53b90489322
Joan Siabato <jsiabato1@unisangil.edu.co>
2020-05-29 15:49:15 -0500

    Aplicación Sever Side Rendering

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import webpack from 'webpack';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { renderRoutes } from 'react-router-config';
+import { StaticRouter } from 'react-router-dom';
+import serverRoutes from '../frontend/routes/serverRoutes';
+import reducer from '../frontend/reducers';
+import initialState from '../frontend/initialState';
 
 dotenv.config();
 
@@ -20,8 +29,8 @@ if (ENV === 'development') {
   app.use(webpackHotMiddleware(compiler));
 }
 
-app.get('*', (req, res) => {
-  res.send(`
+const setResponse = (html, preloadedState) => {
+  return (`
     <!DOCTYPE html>
     <html>
       <head>
@@ -29,12 +38,31 @@ app.get('*', (req, res) => {
         <title>Platzi Video</title>
       </head>
       <body>
-        <div id="app"></div>
+        <div id="app">${html}</div>
+        <script>
+          window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
+        </script>
         <script src="assets/app.js" type="text/javascript"></script>
       </body>
     </html>
   `);
-});
+};
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const preloadedState = store.getState();
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>,
+  );
+
+  res.send(setResponse(html, preloadedState));
+};
+
+app.get('*', renderApp);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
